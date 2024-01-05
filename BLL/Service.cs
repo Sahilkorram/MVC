d5bf40e4e9549b52fbcb96cde03d8a0600280185
Sahilkorram <144720737+Sahilkorram@users.noreply.github.com>
2024-01-05 17:05:44 +0530

    Add files via upload

---
+++ BLL/Service.cs
@@ -0,0 +1,19 @@
+﻿namespace BLL;
+
+using System;
+using BOL;
+using DAL;
+
+
+public class Service
+{
+    public static List<Employee> getalllist()
+    {
+        return DBManager.getalllist();
+    }
+
+    public static bool updateEmp(int eid, string ename, string edate, string edescription,int ehours,string estatus)
+    {
+        return DBManager.updateEmp(eid,ename,edate,edescription,ehours,estatus);
+    }
+}
