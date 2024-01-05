65290e63b7d654aed05da8770cc77495bc013660
Sahilkorram <144720737+Sahilkorram@users.noreply.github.com>
2024-01-05 17:06:23 +0530

    Add files via upload

---
+++ BOL/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+
+namespace BOL;
+
+public class Employee
+{
+    public int eid {get;set;}
+    public string ename{get;set;}
+
+    public string edate{get;set;}
+
+    public string edescription{get;set;}
+
+    public int ehours{get;set;}
+
+    public string estatus{get;set;}
+
+    public Employee(){
+
+    }
+
+    public Employee(int eid,string ename,string edate,string edescription,int ehours,string status){
+        this.eid=eid;
+        this.ename=ename;
+        this.edate=edate;
+        this.edescription=edescription;
+        this.ehours=ehours;
+        this.estatus=estatus;
+    }
+
+}
